b5b863bc2555e5995a8a185f1510d306b0727e4a
fin1taylor <fin1taylor@gmail.com>
2022-07-16 01:51:02 -0700

    small changes
    - smooth scrolling animations (hopefully)
    - changed about me text

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
+// window.addEventListener('scroll', () => {
+//     document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+//   }, false);
   
+;(function() {
+    var throttle = function(type, name, obj) {
+        var obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+    throttle ("scroll", "optimizedScroll");
+})();
+
+var leftgear = document.getElementById("leftgear"),
+rightgear = document.getElementById("rightgear");
+    
+// to use the script *without* anti-jank, set the event to "scroll" and remove the anonymous function.
+
+window.addEventListener("optimizedScroll", function() {
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+});
